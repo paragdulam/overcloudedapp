bf4114b9c4bf531c182162dbab5895145080890a
Parag Dulam <paragdulam@gmail.com>
2016-06-04 14:30:55 +0530

    Commiting necessary files

---
+++ Overclouded/AppManager.swift
@@ -0,0 +1,9 @@
+//
+//  AppManager.swift
+//  Overclouded
+//
+//  Created by Parag Dulam on 04/06/16.
+//  Copyright © 2016 Parag Dulam. All rights reserved.
+//
+
+import Foundation
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, "8.0"
+use_frameworks!
+
+
+target 'Overclouded' do
+    pod 'FBSDKShareKit', '~> 4.12'
+    pod 'FBSDKCoreKit', '~> 4.12'
+end

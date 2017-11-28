27c97f23aee805d76ca703abebb95e6de801a2ff
parag <paragdulam@gmail.com>
2017-11-28 19:18:12 +0530

    First Commit

--- Overclouded/CenterViewController.swift
+++ Overclouded/CenterViewController.swift
@@ -33,7 +33,7 @@ class CenterViewController: UIViewController {
     /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
